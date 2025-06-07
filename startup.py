cef78c94ce4e5bf479e4dbf9a1389fb8ccac77a4
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-07 17:37:06 -0300

    Recria superusuário com campo nome

--- startup.py
+++ startup.py
@@ -2,22 +2,20 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-# Define o módulo de configurações (ajuste se estiver em outro ambiente)
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.settings.prod')
 django.setup()
 
 User = get_user_model()
 
 email = "brunodossantosjardim@gmail.com"
-senha = "Bj101020"
-nome = "Bruno"
+senha = "Bj101200"
+nome = "Bruno Jardim"
 
-# Força a exclusão de superusuário existente com mesmo e-mail
-
+# Deleta usuário anterior com mesmo e-mail (se houver)
 User.objects.filter(email=email).delete()
 
-# Cria novo superusuário
-
+# Cria superusuário com todos os campos obrigatórios
 User.objects.create_superuser(email=email, password=senha, nome=nome)
-print("Superusuário recriado com sucesso.")
+
+# Log para confirmar
+print("Superusuário criado com sucesso!")
