4468f0b1c3cbe8e9f231e36b0e8b5425fa059465
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-07 17:17:19 -0300

    Força recriação do superusuário no deploy

--- startup.py
+++ startup.py
@@ -2,17 +2,22 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
+# Define o módulo de configurações (ajuste se estiver em outro ambiente)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.settings.prod')
 django.setup()
 
 User = get_user_model()
 
 email = "brunodossantosjardim@gmail.com"
-senha = "Bj101200"
+senha = "Bj101020"
 nome = "Bruno"
 
-if not User.objects.filter(email=email).exists():
-    User.objects.create_superuser(email=email, password=senha, nome=nome)
-    print("Superusuário criado com sucesso.")
-else:
-    print("Superusuário já existe.")
+# Força a exclusão de superusuário existente com mesmo e-mail
+
+User.objects.filter(email=email).delete()
+
+# Cria novo superusuário
+
+User.objects.create_superuser(email=email, password=senha, nome=nome)
+print("Superusuário recriado com sucesso.")
