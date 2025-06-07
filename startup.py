75537db869104c0eac6375b3c52edae963078009
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-07 16:55:32 -0300

    Adiciona criação automática de superusuário

--- startup.py
+++ startup.py
@@ -1,18 +1,17 @@
 import os
 import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.settings.prod')
-
-django.setup()
-
 from django.contrib.auth import get_user_model
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.settings.prod')
+django.setup()
+
 User = get_user_model()
-email = 'brunodossantosjardim@gmail.com'
-senha = 'Bj101200'
+
+email = "brunodossantosjardim@gmail.com"
+senha = "admin123"
 
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(email=email, password=senha)
-    print(f"Superusuário '{email}' criado com sucesso.")
+    print("Superusuário criado com sucesso.")
 else:
-    print(f"Superusuário '{email}' já existe.")
+    print("Superusuário já existe.")
