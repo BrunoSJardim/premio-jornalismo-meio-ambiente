6c105701ecc95f8855b725a7dc32e6bcc1f24cd2
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-08 20:29:27 -0300

    Configura armazenamento de arquivos na AWS S3

--- meu_projeto/settings/prod.py
+++ meu_projeto/settings/prod.py
@@ -1,7 +1,9 @@
 from .base import *
+from decouple import config
 import dj_database_url
 
 DEBUG = False
+
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split()
 
@@ -11,3 +13,14 @@ DATABASES = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False  # URLs públicas sem token
