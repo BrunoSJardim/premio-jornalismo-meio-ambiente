9d4722cd8a7f0a5d35f2cd7bbac4755b5e07d4d2
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-10 17:42:57 -0300

    Método save

--- meu_projeto/storages.py
+++ meu_projeto/storages.py
@@ -3,3 +3,13 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class PublicMediaStorage(S3Boto3Storage):
     default_acl = 'public-read'
     querystring_auth = False
+
+    def _save(self, name, content):
+        content.seek(0)  # Garante leitura do início
+        name = super()._save(name, content)
+        self.connection.meta.client.put_object_acl(
+            ACL='public-read',
+            Bucket=self.bucket_name,
+            Key=self._normalize_name(self._clean_name(name)),
+        )
+        return name
