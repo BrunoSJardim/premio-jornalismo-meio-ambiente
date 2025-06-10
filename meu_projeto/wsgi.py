32dfcbb5ff3babad4187377e359985a6991992af
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-10 16:37:01 -0300

    Corrige o direciomanento de arquivos para o AmazonS3

--- meu_projeto/wsgi.py
+++ meu_projeto/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meu_projeto.settings.prod')
 
 application = get_wsgi_application()
