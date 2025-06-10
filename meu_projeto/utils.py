45372eb028b2de64d9edb971a0d41c947e5c3dd3
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-10 19:52:36 -0300

    Correcoes

--- meu_projeto/utils.py
+++ meu_projeto/utils.py
@@ -4,11 +4,11 @@ def salvar_arquivo_com_acl(path, content):
     storage = S3Boto3Storage()
     saved_path = storage.save(path, content)
 
-    # Aplica ACL public-read após o upload
+    # ACL pública
     storage.connection.meta.client.put_object_acl(
         ACL='public-read',
         Bucket=storage.bucket_name,
-        Key=storage._normalize_name(storage._clean_name(saved_path)),
+        Key=storage._normalize_name(saved_path),  # `_clean_name` não é mais necessário
     )
 
     return storage.url(saved_path)
