1ddbb7da7fcd796e82a896c6b262fa68041862e4
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-10 20:33:49 -0300

    Correcoes

--- meu_projeto/settings/prod.py
+++ meu_projeto/settings/prod.py
@@ -14,7 +14,7 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DEFAULT_FILE_STORAGE = 'meu_projeto.storages.PublicMediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
