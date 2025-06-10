472d38f75baa2acd6854193a8f3d295678d814e4
Bruno Jardim <brunodossantosjardim@gmail.com>
2025-06-10 17:34:49 -0300

    Add storages.py

--- meu_projeto/settings/prod.py
+++ meu_projeto/settings/prod.py
@@ -14,7 +14,7 @@ DATABASES = {
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'meu_projeto.storages.PublicMediaStorage '
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -22,7 +22,5 @@ AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-1")
 
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'  # ou None se você quiser acesso restrito
-AWS_QUERYSTRING_AUTH = False     # URLs públicas
 
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
